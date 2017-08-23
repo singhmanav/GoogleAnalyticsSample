4ae3df1152efd4d3c4df4a2eed0cb3f64a8d2162
Manav <manav@xebia.com>
2017-08-23 15:55:10 +0530

    Tab Bar View Controller Added

--- GoogleAnalyticsSample/GoogleAnalyticsSample/ViewController.swift
+++ GoogleAnalyticsSample/GoogleAnalyticsSample/ViewController.swift
@@ -8,18 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITabBarController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
---
+++ GoogleAnalyticsSample/GoogleAnalyticsSample/iPhoneViewController.swift
@@ -0,0 +1,35 @@
+//
+//  iPhoneViewController.swift
+//  GoogleAnalyticsSample
+//
+//  Created by Manav on 23/08/17.
+//  Copyright © 2017 Manav. All rights reserved.
+//
+
+import UIKit
+
+class iPhoneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
